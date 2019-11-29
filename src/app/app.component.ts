30b386c5b0d0374b442621aba5602db731750860
MatteoMit <matteo.mitrano@3mlinformatica.it>
2019-11-29 13:04:31 +0100

    2 files modified, 1 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,29 @@
-import { Component } from "@angular/core";
+import { Component, ViewChild, ElementRef } from "@angular/core"; //
+1;
 @Component({
   selector: "simple-root",
   template: `
-    <simple-car-details #car></simple-car-details>
-    <a href="#">Acquista ora una {{ car.car.model }}</a>
+    <!-- 2: Applichiamo #eur -->
+    <input type="text" placeholder="Inserisci il valore in euro" #eur />
+    <button (click)="convert()">Converti</button>
+    <!-- 3: Applichiamo #conversionRate -->
+    <p>Tasso di conversione EUR/USD <span #conversionRate>1.1379</span></p>
+    <br />
+    {{ usd | currency }}
   `
 })
-export class AppComponent {}
+export class AppComponent {
+  // 4 Utilizziamo il decoratore @ViewChild
+  @ViewChild("eur") eurInputField: ElementRef;
+  @ViewChild("conversionRate") conversionRate: ElementRef;
+  usd = 0;
+  convert() {
+    const eur = parseFloat(this.eurInputField.nativeElement.value);
+    const conversionRate = parseFloat(
+      this.conversionRate.nativeElement.textContent
+    );
+    if (eur) {
+      this.usd = eur * conversionRate;
+    }
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,10 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
-import { CarDetailsComponent } from './car-details.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent , CarDetailsComponent],
+  declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
