d1a6ef17a740108fb773b4d16ea154931bbf107a
MatteoMit <matteo.mitrano@3mlinformatica.it>
2019-11-29 12:58:19 +0100

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,21 +2,14 @@ import { Component } from "@angular/core";
 @Component({
   selector: "simple-root",
   template: `
-    <input type="text" 
-      placeholder="Inserisci il valore in euro" #money />
-    
-    <!-- accediamo al valore del campo input attraverso -->
-    <!-- la Template reference variable #money -->
-
-    <button (click)="convert(money.value)">Converti</button>
-    <br>
-      {{ usd | currency }}
+    <simple-car-details #car></simple-car-details>
+    <a href="#">Acquista ora una {{ car.car.model }}</a>
   `
 })
 export class AppComponent {
   private conversionRate = 1.1379;
   usd = 0;
-  
+
   convert(value: string) {
     this.usd = parseFloat(value) * this.conversionRate;
   }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { CarDetailsComponent } from './car-details.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent , CarDetailsComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
